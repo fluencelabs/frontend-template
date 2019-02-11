f98e7f832dfc0777c516def3a664d8d39de4d93a
folex <0xdxdy@gmail.com>
2019-02-11 17:53:29 +0300

    Use fluence 0.1.6

--- index.js
+++ index.js
@@ -1,18 +1,20 @@
 import * as fluence from "fluence";
 
 // address to Fluence contract in Ethereum blockchain. Interaction with blockchain created by MetaMask or with local Ethereum node
-let contractAddress = "0xe9bbe60d525c7c5d4f3d85036f3ea23003879106";
+let contractAddress = "0x5faa7b8d290407460e0ec8585b2712acf27290f9";
+
+let ethUrl = "http://46.101.213.180:8545"
 
 // application to interact with that stored in Fluence contract
-let appId = "<put your app id here>";
+let appId = "1";
 
-let fluenceSession;
+window.fluence = fluence;
 
 // creates a session between client and backend application
-fluence.createAppSession(contractAddress, appId).then((s) => {
+fluence.connect(contractAddress, appId, ethUrl).then((s) => {
 		console.log("Session created");
-		window.fluenceSession = s;
-		fluenceSession = s;
+		window.session = s;
+		session = s;
 });
 
 // gets a result and logs it
